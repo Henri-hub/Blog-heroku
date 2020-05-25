c5708fcf610728a23ae10977d4567819904b9fb8
Henri NSUKULA <nnsukula.carine@yahoo.com>
2020-05-25 14:47:57 +0200

    ajout

---
+++ middleware/redirectAuthSucess.js
@@ -0,0 +1,13 @@
+const User = require('../database/models/User')
+
+module.exports = (req, res, next) => {
+       
+    
+        if(req.session.userId) {
+
+            return res.redirect('/articles/add')
+        }
+
+        next()
+}
+
